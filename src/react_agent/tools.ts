4096fd09d0d41ccfd1bff461c469aec4a13fc972
Jake Croft <jakedrc1895@gmail.com>
2025-07-07 12:21:02 -0600

    Update tools.ts

--- src/react_agent/tools.ts
+++ src/react_agent/tools.ts
@@ -64,7 +64,7 @@ const stateRegisterTool = tool(
     try {
     
       const response = await fetch(
-        `https://d4dd-205-170-56-18.ngrok-free.app/creds/good-standing/company?company=${input.companyname}&state=${input.state}`
+        `https://0eec-73-169-47-136.ngrok-free.app/creds/good-standing/company?company=${input.companyname}&state=${input.state}`
       );
 
       if (!response.ok) {
@@ -97,7 +97,7 @@ const DomainVerificationSchema = z.object({
 const DomainVerificationTool = tool(
   async (input): Promise<string> => {
     const response = await fetch(
-      `https://d4dd-205-170-56-18.ngrok-free.app/creds/good-standing/domain?domain=${input.domain}`
+      `https://0eec-73-169-47-136.ngrok-free.app/creds/good-standing/domain?domain=${input.domain}`
     );
     const data = await response.json();
     return data;
@@ -117,7 +117,7 @@ const websiteVerificationSchema = z.object({
 const websiteVerificationTool = tool(
   async (input): Promise<string> => {
     const response = await fetch(
-      `https://d4dd-205-170-56-18.ngrok-free.app/creds/good-standing/website?website=${input.website}`
+      `https://0eec-73-169-47-136.ngrok-free.app/creds/good-standing/website?website=${input.website}`
     );
     const data = await response.json();
     return data;
@@ -138,7 +138,7 @@ const emailVerificationSchema = z.object({
 const emailVerificationTool = tool(
   async (input): Promise<string> => {
     const response = await fetch(
-      `https://d4dd-205-170-56-18.ngrok-free.app/creds/good-standing/domain?domain=${input.email}`
+      `https://0eec-73-169-47-136.ngrok-free.app/creds/good-standing/domain?domain=${input.email}`
     );
     const data = await response.json();
     return data;

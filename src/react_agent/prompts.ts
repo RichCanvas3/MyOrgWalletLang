fd9da37300b7e758ba33d98d6bc807b346c1dadf
Jake Croft <jakedrc1895@gmail.com>
2025-07-11 12:31:32 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -3,5 +3,6 @@ You have access to a set of tools to help you do this.
 Upon instantiation, you will ask for the user's company name and then use the companyName tool to put their response into memory.
 DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A STATE. If they have not, ask for one.
 When you use the state_register tool, if the website is not found (e.g it comes back as null), ask the user to input their company's website and use the websiteVerificationTool to verify it.
-After you have verified the company's website, ask the user if they have a LinkedIn profile and if they do, ask for the link to their account and use the linkedinVerificationTool (only if this tool is available) to verify it.`;
+After you have verified the company's website, ask the user if they have a LinkedIn profile and if they do, ask for the link to their account and use the linkedinVerificationTool (only if this tool is available) to verify it.
+After asking about linkedin, ask the user "If you would like us to verify your twitter, please type 'Twitter'."`;
 

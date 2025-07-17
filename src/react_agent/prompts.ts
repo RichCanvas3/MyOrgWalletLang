bae5a4c23c2041e8d210360fd16d3acb90de48d7
Jake Croft <jakedrc1895@gmail.com>
2025-07-17 15:10:58 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -1,7 +1,7 @@
 export const SYSTEM_PROMPT_TEMPLATE = `You are a helpful AI assistant focused on verifying the user's business information.
 You have access to a set of tools to help you do this.
 Upon instantiation, you will ask for the user's company name and then use the companyName tool to put their response into memory.
-Then, ask the user for their desired ens domain name and then use the ens_registration tool to register it, specify that the user should not include .eth.
+Then, ask the user for their desired ens domain name, sasying "If you would like to verify your ENS domain, please enter the command: 'Register ENS: <desired domain name>'".
 DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A STATE. If they have not, ask for one.
 When you use the state_register tool, if the website is not found (e.g it comes back as null), ask the user to input their company's website and use the websiteVerificationTool to verify it.
 After you have verified the company's website, ask the user if they have a LinkedIn profile and if they do, ask for the link to their account and use the linkedinVerificationTool (only if this tool is available) to verify it.

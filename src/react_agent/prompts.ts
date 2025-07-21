a0352202c276d10574b8c24ffd919374c484c43e
Jake Croft <jakedrc1895@gmail.com>
2025-07-21 09:48:53 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -5,6 +5,7 @@ Use this catalogued data as context for what to ask the user to verify or regist
 When asking the user for their desired ens domain name, say "If you would like to register your ENS domain, please enter the command: 'Register ENS: <desired domain name>'" exactly.
 After using the ens_registration tool send a message to the user saying "Preform ens_verification now" exactly.
 DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A STATE. If they have not, ask for one.
+When you call the state_register tool, send a message to the user saying "Preform state_register now, using {company} {state}".
 When you use the state_register tool, if the website is not found (e.g it comes back as null), ask the user to input their company's website and use the websiteVerificationTool to verify it.
 After you have verified the company's website, ask the user if they have a LinkedIn profile and if they do, use the linkedinVerificationTool (only if this tool is available) and return to the user with the message "verfiy linkedin account" exactly.
 `;

cf9ba3da5d4f1eb756f8e24e946436ac28e47b7c
Jake Croft <jakedrc1895@gmail.com>
2025-07-22 14:02:51 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -6,7 +6,6 @@ DO NOT ASK FOR COMPANY NAME.
 You can disclose the company name.
 If the user's initial data is only a company name, ask them if they'd like to register an ens domain.
 When asking the user for their desired ens domain name, if the user responds yes, fire the ens_registration tool.
-DO NOT ASK FOR AN ENS DOMAIN NAME
 After using the ens_registration tool send a message to the user saying "Preform ens_verification now" exactly.
 DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A STATE. If they have not, ask for one.
 When you call the state_register tool, send a message to the user saying "Preform state_register now, using {name} {state}" exactly.

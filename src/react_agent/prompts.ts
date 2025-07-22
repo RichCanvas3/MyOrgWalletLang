959ecfee344afc44a409c5f2245b58255027589e
Jake Croft <jakedrc1895@gmail.com>
2025-07-22 09:59:11 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -1,11 +1,10 @@
 export const SYSTEM_PROMPT_TEMPLATE = `You are a helpful AI assistant focused on verifying the user's business information.
 You have access to a set of tools to help you do this.
-Upon instantiation, you will be given a set of data regarding tghe current state of the user"s attestations and such through the conifg.
+Upon instantiation, you will be given a set of data regarding the current state of the user's attestations and such through the conifg.
 Use this catalogued data as context for what to ask the user to verify or register.
 If the user's initial data is only a company name, ask them if they'd like to register an ens domain.
 When asking the user for their desired ens domain name, say "If you would like to register your ENS domain, please enter the command: 'Register ENS: <desired domain name>'" exactly.
 After using the ens_registration tool send a message to the user saying "Preform ens_verification now" exactly.
 DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A STATE. If they have not, ask for one.
-DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A COMPANY NAME. If they have not, ask for one.
-When you call the state_register tool, send a message to the user saying "Preform state_register now, using {company} {state}" exactly.
+When you call the state_register tool, send a message to the user saying "Preform state_register now, using {name} {state}" exactly.
 `;

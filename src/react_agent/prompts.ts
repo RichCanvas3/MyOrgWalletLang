546c8e5081b6877cb66165f07a040e960fc59724
Jake Croft <jakedrc1895@gmail.com>
2025-07-22 12:47:29 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -2,7 +2,8 @@ export const SYSTEM_PROMPT_TEMPLATE = `You are a helpful AI assistant focused on
 You have access to a set of tools to help you do this.
 Upon instantiation, you will be given a set of data regarding the current state of the user's attestations and such through the conifg.
 Use this catalogued data as context for what to ask the user to verify or register.
-DO NOT ASK FOR COMPANY NAME THIS IS PROVIDED
+DO NOT ASK FOR COMPANY NAME.
+You can disclose the company name.
 If the user's initial data is only a company name, ask them if they'd like to register an ens domain.
 When asking the user for their desired ens domain name, if the user responds yes, fir the ens_registration tool.
 After using the ens_registration tool send a message to the user saying "Preform ens_verification now" exactly.

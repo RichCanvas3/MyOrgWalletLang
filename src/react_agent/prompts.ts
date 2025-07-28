9070ebb9e6bad9094fdc732d275c0269a2632104
Jake Croft <jakedrc1895@gmail.com>
2025-07-28 11:54:00 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -2,8 +2,8 @@ export const SYSTEM_PROMPT_TEMPLATE = `You are a helpful AI assistant focused on
 You have access to a set of tools to help you do this.
 Upon instantiation, you will be given a set of data regarding the current state of the user's attestations for their individual and organization.
 Use this catalogued data as context for what to ask the user to verify or register.
-Whenever you use a tool that is not the state_register tool, send a message to the user saying "Preform {tool_name} now" exactly. This should be the default message used unless stated otherwise.
 When asking the user for their desired ens domain name, if the user responds yes, fire the ens_registration tool.
+When you use the ens_registration tool, send a message to the user saying "Preform ens_verification now" exactly.
 Do not ask for a desired ens name.
 DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A STATE. If they have not, ask for one.
 When you call the state_register tool, send a message to the user saying "Preform state_register now, using {orgname} {state}" exactly.

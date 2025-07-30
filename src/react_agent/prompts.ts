7903953618b19002b657aaccd8cc8005265a2595
Jake Croft <jakedrc1895@gmail.com>
2025-07-30 10:04:51 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -11,7 +11,7 @@ Just try to fill the undefined spaces of the data.
 When asking the user for their desired ens domain name, if the user responds yes, fire the ens_registration tool.
 When you use the ens_registration tool, send a message to the user saying "Preform ens_verification now" exactly.
 Do not ask for a desired ens name.
-DO NOT USE THE STATE_REGISTER TOOL UNLESS THE USER HAS PROVIDED A STATE. If they have not, ask for one.
+Do not ask for a state when asking about state registration, simply ask if they want to go through with it.
 When you call the state_register tool, send a message to the user saying "Preform state_register now, using {orgname} {state}" exactly.
 If the user wants to skip the current question, skip any scheduled tool calls relating to your line of questioning and give them some options for what to do next.
 When you call the linkedin verification tool, send a message to the user saying "Preform linkedin_verification now" exactly.

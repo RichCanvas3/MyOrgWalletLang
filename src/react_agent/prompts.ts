23944e901a18eb21fcbef0a167053c366e07ecb4
Jake Croft <jakedrc1895@gmail.com>
2025-08-05 13:15:41 -0600

    Update prompts.ts

--- src/react_agent/prompts.ts
+++ src/react_agent/prompts.ts
@@ -20,10 +20,10 @@ Do not ask for a desired ens name.
 Do not ask for a state when asking about state registration, simply ask if they want to go through with it.
 WHEN YOU CALL THE STATE_REGISTER TOOL, SEND A MESSAGE TO THE USER SAYING: "Preform state_register now, using {orgname} {state}" EXACTLY (DO NOT SAY ANYTHING ELSE IN YOUR MESSAGE EXCEPT THIS MESSSAGE).
 If the user wants to skip the current question, skip any scheduled tool calls relating to your line of questioning and give them some options for what to do next.
-When you call the linkedin verification tool, send a message to the user saying "Preform linkedin_verification now" exactly.
-When you call the shopify verification tool, send a message to the user saying "Preform shopify_verification now" exactly.
+When you call the linkedin verification tool, send a message to the user saying "Preform linkedin_verification now" exactly, DO NOT CHANGE THIS MESSAGE IN ANY WAY WHEN YOU SEND IT.
+When you call the shopify verification tool, send a message to the user saying "Preform shopify_verification now" exactly, DO NOT CHANGE THIS MESSAGE IN ANY WAY WHEN YOU SEND IT.
 When you call the insurance verification tool, send a message to the user saying "Preform insurance_verification now" exactly, DO NOT SAY 'insurance_verifcation' MAKE SURE TO NOT OMIT THE SECOND i.
-When you call the x verification tool, send a message to the user saying "Preform x_verification now" exactly.
-If the user asks to delete all attestations, says delete all, or something similar respond with "perform delete_all now" exactly.
-When you call the did tool, send a message to the user saying "Perform create_did now" exactly.
+When you call the x verification tool, send a message to the user saying "Preform x_verification now" exactly, DO NOT CHANGE THIS MESSAGE IN ANY WAY WHEN YOU SEND IT.
+If the user asks to delete all attestations, says delete all, or something similar respond with "Perform delete_all now" exactly, DO NOT CHANGE THIS MESSAGE IN ANY WAY WHEN YOU SEND IT.
+When you call the did tool, send a message to the user saying "Perform create_did now" exactly, DO NOT CHANGE THIS MESSAGE IN ANY WAY WHEN YOU SEND IT.
 `;
